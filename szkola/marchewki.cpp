ae60bba5d3ac6d53e63681a80bf53730bdd53303
Tomuus <tomasz1.mrozinski@gmail.com>
2025-05-19 18:46:59 +0200

    Dodanie jakis ktore nie przeszly przy ostatnim
    Marchewki i Wedki
    Dzieki Gorgiel ze zauwazyles

--- szkola/marchewki.cpp
+++ szkola/marchewki.cpp
@@ -6,27 +6,32 @@ using namespace std;
 long long pkt(long long r)
 {
     long long ans = 0;
-    for (long long x = -r; x <= r; ++x)
+    for (long long x = 0; x <= r; ++x)
     {
-        long long troj=(long long)floor(sqrt((long double)r * r - (long double)x * x));
-        ans+=troj+1;
+        long long y_limit = (long long)floor(sqrt((long double)r * r - (long double)x * x));
+        ans += 4 * y_limit + 1;
     }
-    return ans*4 - 4*r - 3;
+    return ans-r;
+}
+
+
+long long bins(long long s,long long p,long long l)
+{
+    long long m=(p+l)/2;
+    if(l<p) return p;
+    if(pkt(m)==s) return m;
+    if(pkt(m)>s) return bins(s,p,m-1);
+    else return bins(s,m+1,l);
 }
 
 
 int main()
 {
+    ios_base::sync_with_stdio(0);
+    //cin.tie(0);
     long long n;
-    //cout<<pkt(8765543);
+    //cout<<pkt(2);
     cin >> n;
-    int lewy=0,prawy=(long long)ceil(sqrt(n / M_PI)) + 1000;
-    while(lewy<prawy)
-    {
-        long long m=(lewy+prawy)/2;
-        if(pkt(m)>=n) prawy=m;
-        else lewy=m+1;
-    }
-    cout<<lewy;
+    cout<<bins(n,0,1000000);
     return 0;
 }
--- szkola/wedki.cpp
+++ szkola/wedki.cpp
@@ -1,37 +1,39 @@
 #include <iostream>
-#include <cmath>
+#include <vector>
 
 using namespace std;
 
-long long pkt(long long r)
+long long dziel(vector <long long> tab, long long goal, long long n)
 {
-    long long ans = 0;
-    for (long long x = 0; x <= r; ++x)
+    long long ans=1,last=tab[0];
+    for(int i=1;i<n;i++)
     {
-        long long y_limit = (long long)floor(sqrt((long double)r * r - (long double)x * x));
-        ans += 4 * y_limit + 1;
+        if(tab[i]-last>=goal)
+        {
+            last=tab[i];
+            ans++;
+        }
     }
-    return ans-r;
+    return ans;
 }
-
-
-long long bins(long long s,long long p,long long l)
-{
-    long long m=(p+l)/2;
-    if(l<p) return p;
-    if(pkt(m)==s) return m;
-    if(pkt(m)>s) return bins(s,p,m-1);
-    else return bins(s,m+1,l);
-}
-
 
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    long long n;
-    //cout<<pkt(2);
-    cin >> n;
-    cout<<bins(n,0,1000000);
-    return 0;
+    long long i,n,m,tmp,prev=0;
+    cin>>n>>m;
+    vector <long long> tab(n);
+    for(i=0;i<n;i++)
+    {
+        cin>>tab[i];
+    }
+    //cout<<dziel(tab,7,n);
+    long long p=1000000000,l=0;
+    while(p>l)
+    {
+        long long mid=(p+l+1)/2;
+        if(dziel(tab,mid,n)>=m) l=mid;
+        else p=mid-1;
+    }
+    if(p==0) cout<<1;
+    else cout<<p;
 }
