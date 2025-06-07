02458bd2345e7f167bf70e59aa87312607849779
Tomuus <tomasz1.mrozinski@gmail.com>
2025-06-08 00:05:01 +0200

    Zadania z lca
    Udalo mi sie zrobic ten LCA(czarna magia) i jakies zadanka z tego
    Pewnie jutro cos jeszcze ogarne
    Musze sobie znalesc cos poza zadankami do robienia

---
+++ algorytmy/max_przedzial.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+//jezu to jest jakas czarna magia
+//dobra zaraz to ogarne i naucze sie moze jakos z pamieci pisac
+
+using namespace::std;
+
+typedef long long lo; //niemozliwe czyzbym uzyl mozgu i nauczyl sie czegos nowego
+
+const long long big=1e9;
+long long tab[(1<<21) + 7];
+long long kolejki[(1<<21)+7];
+long long ll=1;
+
+void push(lo l, lo p, lo k)
+{
+    if(kolejki[k]==0) return;
+    lo mid =(l+p)/2;
+    tab[k*2]+= kolejki[k];
+    tab[2*k+1] += kolejki[k];
+    kolejki[k*2]+=kolejki[k];
+    kolejki[k*2+1]+=kolejki[k];
+    kolejki[k]=0;
+}
+
+void apdejt (lo k, lo l, lo p, lo ol, lo op, lo v)
+{
+    if(ol>p or op<l) return;
+    if(ol<=l and p<=op)
+    {
+        tab[k]+=v;
+        kolejki[k]+=v;
+        return;
+    }
+    push(l,p,k);
+    int mid=(l+p)/2;
+    apdejt (k*2,l,mid,ol,op,v);
+    apdejt (k*2+1,mid+1,p,ol,op,v);
+    tab[k]=max(tab[k*2],tab[k*2+1]);
+}
+
+long long przedzial(long long p, long long k, long long a, long long b, long long i) {
+    if (p <= a && b <= k ) return tab[i]; //dokladnie wewnatrz
+    if (b<p or a>k) return 0; //zly przedzial
+    push(a,b,i);
+    long long mid = (a + b) / 2; //cos miedzy
+    return max(przedzial(p, k, a, mid, i * 2) , przedzial(p, k, mid+1, b, i * 2 + 1));
+}
+
+int main()
+{
+    fill(tab, tab + ((1<<21)+7), 0);
+    long long i,n,m,a,b,c,p;
+    cin>>n>>p>>m;
+    while(ll<n) ll*=2;
+    /*for(i=0;i<n;i++) cin>>tab[ll+i]; //wczytywanie
+    for(i=ll-1;i>0;i--) tab[i]=tab[i*2]tab[i*2+1]; //dopelnianie */
+    //for(i=0;i<n+5;i++) cout<<tab[i]<<' ';
+    //cout<<przedzial(0,n-1,0,ll-1,1)<<endl;
+    for(i=0;i<m;i++)
+    {
+        cin>>a>>b>>c;
+        a--; b--;
+        if(przedzial(a,b-1,0,ll-1,1)+c>p) cout<<"N";
+        else
+        {
+            cout<<'T';
+            apdejt(1,0,ll-1,a,b-1,c);
+        }
+        cout<<endl;
+    }
+}
