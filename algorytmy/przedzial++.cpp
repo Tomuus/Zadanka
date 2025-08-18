6031f1aadbd774b9853ab63c295270652ec8cdfd
Tomus <tomasz1.mrozinski@proton.me>
2025-08-18 16:22:34 +0200

    Dokumentacja drzew predzial-przedzial
    Ten magiczny moment kiedy patrze sie na swoj stary kod
    I nagle widze ze nic nie rozumiem
    Zauwazam komentarz ze chcialem umiec to z pamieci
    I 20 jednoliterowych zmiennych
    Nagle moja swiadomosc ze jestem idiota wzrasta 100 razy
    
    Btw jestem teraz na staszicowym obozie informatycznym
    Potem zcommituje zadanka z niego
    I zaklepalem slowackiego++

--- algorytmy/przedzial++.cpp
+++ algorytmy/przedzial++.cpp
@@ -2,6 +2,7 @@
 
 //jezu to jest jakas czarna magia
 //dobra zaraz to ogarne i naucze sie moze jakos z pamieci pisac
+//Kurw cos mnie pieklo jezeli wierzylem ze bede to robil z pamieci
 
 using namespace::std;
 
@@ -14,35 +15,41 @@ long long ll=1;
 
 void push(lo l, lo p, lo k)
 {
-    if(kolejki[k]==0) return;
+    if(kolejki[k]==0) return; //Jezeli brak zadan koniec
     lo mid =(l+p)/2;
-    tab[k*2]+= kolejki[k]*(mid-l+1);
+    tab[k*2]+= kolejki[k]*(mid-l+1); //Aktualizacja dzieci
     tab[2*k+1] += kolejki[k]*(p-mid);
-    kolejki[k*2]+=kolejki[k];
+    kolejki[k*2]+=kolejki[k]; //Danie dzieciom zadania aktualizacji
     kolejki[k*2+1]+=kolejki[k];
     kolejki[k]=0;
 }
 
 void apdejt (lo k, lo l, lo p, lo ol, lo op, lo v)
+//Rozpis zmiennych
+//k - miejsce w tablicy
+//l, p - rozwarzany przedzial
+//ol, op - docelowy przedzial
+//v - o ile zmienic
 {
-    if(ol>p or op<l) return;
-    if(ol<=l and p<=op)
+    if(ol>p or op<l) return; //Jezeli nie nalezy do przedizalu do aktualizacji, omin
+    if(ol<=l and p<=op) //Jezeli nalezy, aktualizuj
     {
-        tab[k]+=v*(p-l+1);
-        kolejki[k]+=v;
+        tab[k]+=v*(p-l+1); //Jako ze aktualizujemy wiele punktow, mnozymy
+        kolejki[k]+=v; //kolejkujemy dalsze zamiany
         return;
     }
-    push(l,p,k);
+    //Jezeli nalezy tylko czesciowo
+    push(l,p,k); //Aktualizuj poprzednie zmiany
     int mid=(l+p)/2;
-    apdejt (k*2,l,mid,ol,op,v);
+    apdejt (k*2,l,mid,ol,op,v); //Wejscie glebiej w dzieci akt. wieszcholka
     apdejt (k*2+1,mid+1,p,ol,op,v);
-    tab[k]=tab[k*2]+tab[k*2+1];
+    tab[k]=tab[k*2]+tab[k*2+1]; //Aktualizacja danego punktu na podstawie zmiany dzieci
 }
 
 long long przedzial(long long p, long long k, long long a, long long b, long long i) {
     if (p <= a && b <= k ) return tab[i]; //dokladnie wewnatrz
     if (b<p or a>k) return 0; //zly przedzial
-    push(a,b,i);
+    push(a,b,i); //Aktoualizacja dzieci
     long long mid = (a + b) / 2; //cos miedzy
     return przedzial(p, k, a, mid, i * 2) + przedzial(p, k, mid+1, b, i * 2 + 1);
 }
